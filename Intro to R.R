a876cb301d825e32ce3c973aceab6e2edd732cbd
Angelo Cerminara <anc225@lehigh.edu>
2021-08-29 15:16:34 -0400

    Added written by
    Written by Angelo Cermianra

--- Intro to R.R
+++ Intro to R.R
@@ -1,5 +1,6 @@
 # R script to accompany Intro to R
 # Copy for CerminaraAngelo 2021
+# Written by Angelo Cerminara
 ######################################################
 
 # You will see that you can put non-executable "comment" lines
